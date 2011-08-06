d2b5c3b05ca8fd771666f815238e9eaedcce6041
Joseph Swan <joeswan@me.com>
2011-08-06 21:35:50 +0100

    Generated Rails app

---
+++ rails/afflitics/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afflitics::Application.config.secret_token = '837e37063d913f045ccad9810f208b9d53ae7df194318f948a425b58142e47df8307a566519448414a13a1fe62a03fe1cd4234bdff087ab8cbf875f6fe9d8459'
