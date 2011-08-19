c87294b062dafda68f4f5e1322bafa6dd6dee008
James Pickup <james@epickup.co.uk>
2011-08-19 04:56:16 +0100

    Removed invalid show spec

--- rails/afflitics/spec/controllers/users_controller_spec.rb
+++ rails/afflitics/spec/controllers/users_controller_spec.rb
@@ -9,11 +9,6 @@ describe UsersController do
 
   describe "GET 'show'" do
 
-    it "should be successful" do
-      get 'show'
-      response.should be_success
-    end
-    
     it "should find the right user" do
       get :show, :id => @user.id
       assigns(:user).should == @user
